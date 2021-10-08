d566a58ae10fdb377dca2b754b2b6245493aa750
keho9876 <keho9876@colorado.edu>
2021-10-08 12:34:33 -0600

    update 10/8

---
+++ JavaScript/script.js
@@ -0,0 +1,30 @@
+console.log("JavaScript Working");
+//  document.getElementById("button1").addEventListener("click", buttonOne);
+//  document.getElementById("button2").addEventListener("click", buttonTwo);
+  //document.getElementById("button3").addEventListener("click", buttonThree);
+
+  function buttonOne(){
+    console.log("clicked buttonOne");
+    alert("Are you sure?");
+  //  document.getElementById("second.html").addEventListener();
+  }
+
+  function buttonTwo(){
+    alert("Are you sure?");
+  //  document.getElementById("third.html").addEventListener();
+  }
+
+  function buttonThree(){
+    alert("Are you sure?");
+//    document.getElementById("fourth.html").addEventListener();
+  }
+
+  function buttonFour(){
+    alert("Are you sure?");
+//    document.getElementById("fourth.html").addEventListener();
+  }
+
+  function buttonFive(){
+    alert("You are Awsome!");
+//    document.getElementById("fourth.html").addEventListener();
+  }
