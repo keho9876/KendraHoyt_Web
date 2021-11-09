fe2482e8d26bce8ef77e5fbbbbe31a3c6b58c2f9
keho9876 <keho9876@colorado.edu>
2021-11-08 22:06:39 -0700

    New Updat 8/11

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ let img;
 
 for (i = 0; i < imgs; i++){
   img = 1+i;
-  document.getElementById('stuff').innerHTML += "<img src=Assets/about/" + img + ".jpg'" + "class='aboutimg' id='img" + img + "' />";
+  document.getElementById('stuff').innerHTML += "<img src= 'about/" + img + ".jpg'" + "class='aboutimg' id='img" + img + "' />";
 }
