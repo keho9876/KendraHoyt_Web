858ad20b84a15e4e37399270f6126b2b70db5495
keho9876 <keho9876@colorado.edu>
2021-11-11 00:23:04 -0700

    update 11/11

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ console.log("Hello World");
 
 let imgs = 6;
 let img;
+// let artworks = 19;
+// let artwork;
 
 for (i = 0; i < imgs; i++){
   img = 1+i;
   document.getElementById('stuff').innerHTML += "<img src= 'about/" + img + ".jpg'" + "class='aboutimg' id='img" + img + "' />";
 }
+
+// for (i = 0; i < artworks; i++){
+//   artwork = 1+i;
+//   document.getElementById('create').innerHTML += "<img src= 'ksartwork/" + artwork + ".jpg'" + "class='creations' id='artwork" + artwork + "' />";
+// }
